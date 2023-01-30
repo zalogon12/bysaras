dc26d6010f0aa18d7e59578431501d3c7b4590a0
zalogon12 <88121194+zalogon12@users.noreply.github.com>
2023-01-30 09:43:24 -0300

    Primer commit, estructura de la pagina

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+//define('URL', 'http://localhost/mvc/');
+define('URL', 'http://' . $_SERVER['HTTP_HOST'] . '/bysaras/');
+
+//conexion a la base de datos
+define('HOST', 'bysaras');
+define('PORT', '3306');
+define('DB', '');
+define('USER', 'root');
+define('PASSWORD', "");
+define('CHARSET', 'utf8mb4');
---
+++ views/header.php
@@ -0,0 +1,21 @@
+<div class="cont-hi">
+    <div class="menu">
+    <div class="cont-br">
+        <ul>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Vestimenta</a></li>
+        <li><a href="#">Accesorios</a></li>
+</ul></div>
+       <div class="redes">
+<a href="#"><img src="public/css/img/fb.png" alt=""></a>
+<a href="#"><img src="public/css/img/ig.png" alt=""></a>
+<a href="#"><img src="public/css/img/gm.png" alt=""></a>
+<a href="#"><img src="public/css/img/wp.png" alt=""></a>      
+</div>
+<div class="logot">
+
+<a href="#"><img src="public/css/img/saralogo.png" alt=""></a>
+       
+    </div>
+</div>
+</div>
